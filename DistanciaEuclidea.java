1f4fcdee8794b2ab16364402b0e6096c10ee4788
acielos <antoniocielos@gmail.com>
2025-10-27 18:40:45 +0100

    DistanciaEuclidea v1

--- DistanciaEuclidea.java
+++ DistanciaEuclidea.java
@@ -1,2 +1,11 @@
 public class DistanciaEuclidea {
+
+    public double distanciaEuclidea(Punto a, Punto b){
+
+        // Calculamos la distancia euclidea entre dos puntos
+        double distancia = Math.sqrt(Math.pow((a.getX() - b.getX()), 2) + Math.pow((a.getY() - b.getY()), 2));
+
+        return distancia;
+
+    }
 }
