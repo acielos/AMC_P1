89ba2636f4704027ff4dc53c08142de37fb77423
acielos <antoniocielos@gmail.com>
2025-10-28 15:38:01 +0100

    Algortimo v4 create Resultado.java

---
+++ Resultado.java
@@ -0,0 +1,12 @@
+public class Resultado {
+
+    private Punto punto1;
+    private Punto punto2;
+    private double distancia;
+
+    public void Resultado(Punto punto1, Punto punto2, double distancia){
+        this.punto1 = punto1;
+        this.punto2 = punto2;
+        this.distancia = distancia;
+    }
+}
