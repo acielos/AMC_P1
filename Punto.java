f4df32cbc9104e536e4e301dca04a0ce31a7446b
acielos <antoniocielos@gmail.com>
2025-10-28 12:03:59 +0100

    Punto update

--- Punto.java
+++ Punto.java
@@ -1,14 +1,19 @@
-import java.util.*;
 public class Punto {
 
     // Declaramos los atributos de la clase punto
+    private int id;
     private double x;
     private double y;
 
     // Declaramos el constructor
     public Punto(int id, double x, double y){
+        this.id = id;
         this.x = x;
         this.y = y;
+    }
+
+    public int getID(){
+        return id;
     }
 
     public double getX(){
@@ -21,7 +26,7 @@ public class Punto {
 
     // Pasamos a String para dar formato a la visualización del punto
     public String toString(){
-        return String.format("(%f, %f)", x, y);
+        return String.format("%d (%f, %f)", id, x, y);
     }
 
 }
