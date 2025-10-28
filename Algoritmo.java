99b549d3175be4a620ace72ac45a254579824a27
acielos <antoniocielos@gmail.com>
2025-10-28 13:02:23 +0100

    Algortimo v1

--- Algoritmo.java
+++ Algoritmo.java
@@ -1,3 +1,17 @@
-public class Algoritmo {
-    //
+import java.util.*;
+
+public abstract class Algoritmo {
+
+    protected List<Punto> dataset;
+    protected double mejor_distancia;
+    protected Punto[] mejor_par;
+    protected DistanciaEuclidea calc_distancia;
+
+    // Implementamos el metodo run que deberán usar todas las clases hijas
+    public abstract void run();
+
+    // Implementamos el metodo mejorPar
+    public void mejorPar(){
+
+    }
 }
